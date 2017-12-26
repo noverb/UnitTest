926bbf8354aab373fc3aa5aa63acb723526a2dfb
Rodolfo Novello <noverb01@hotmail.com>
2017-12-26 16:02:37 -0600

    Added files via upload
    Could not get Xamarin to upload these files

---
+++ MVCAppWUnitTest.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using MVCAppWUnitTest;
+using MVCAppWUnitTest.Controllers;
+
+namespace MVCAppWUnitTest.Controllers
+{
+    [TestFixture]
+    public class HomeControllerTest
+    {
+        [Test]
+        public void Index()
+        {
+            // Arrange
+            var controller = new HomeController();
+
+            // Act
+            var result = (ViewResult)controller.Index();
+
+            var mvcName = typeof(Controller).Assembly.GetName();
+            var isMono = Type.GetType("Mono.Runtime") != null;
+
+            var expectedVersion = mvcName.Version.Major + "." + mvcName.Version.Minor;
+            var expectedRuntime = isMono ? "Mono" : ".NET";
+
+            // Assert
+            Assert.AreEqual(expectedVersion, result.ViewData["Version"]);
+            Assert.AreEqual(expectedRuntime, result.ViewData["Runtime"]);
+        }
+
+        [Test]
+        public void Test02()
+        {
+            Assert.True(true);
+        }
+
+        [Test]
+        public void Test03()
+        {
+            Assert.False(false);
+        }
+    }
+}
